7146d0e304dbf49a1b3ac5fbf907c2f5332e2583
Cong <congusbongus@gmail.com>
2016-01-28 22:00:29 +1100

    Choose server to connect to (fixes #1)

--- client.c
+++ client.c
@@ -14,6 +14,8 @@ ENetHost *start_client(ENetPeer **peer, const int timeout_seconds);
 void send_string(ENetPeer *peer, char *s);
 void stop_client(ENetHost *host, ENetPeer *peer);
 #define CONNECTION_WAIT_MS 5000
+// Arbitrary max number of servers to scan for
+#define MAX_SERVERS 10
 
 
 int main(int argc, char *argv[])
@@ -92,6 +94,8 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
+int find_servers(ServerInfo *server_infos, ENetAddress *addrs, int max_servers, int timeout_seconds);
+
 ENetHost *start_client(ENetPeer **peer, const int timeout_seconds)
 {
 	if (enet_initialize() != 0)
@@ -100,17 +104,87 @@ ENetHost *start_client(ENetPeer **peer, const int timeout_seconds)
 		return NULL;
 	}
 
-	// Scan for server on LAN
+	// Scan for servers on LAN
+	ServerInfo sinfos[MAX_SERVERS];
+	ENetAddress addrs[MAX_SERVERS];
+	int n_servers = find_servers(sinfos, addrs, MAX_SERVERS, timeout_seconds);
+	if (n_servers == 0)
+	{
+		fprintf(stderr, "No servers found\n");
+		return NULL;
+	}
+
+	// If there's more than one server, present a menu and let the user pick
+	int server_index = 0;
+	if (n_servers > 1)
+	{
+		for (int i = 0; i < n_servers; i++)
+		{
+			char buf[256];
+			enet_address_get_host_ip(&addrs[i], buf, sizeof buf);
+			printf("%d - %s (%s:%d)\n", i, sinfos[i].hostname, buf, addrs[i].port);
+		}
+		printf("Connect to server? [0] ");
+		for (;;)
+		{
+			char input[256];
+			if (fgets(input, sizeof input, stdin) == NULL)
+			{
+				break;
+			}
+			int choice = atoi(input);
+			if (choice >= 0 && choice < n_servers)
+			{
+				server_index = choice;
+				break;
+			}
+		}
+	}
+
+	ENetHost *host = enet_host_create(NULL, 1, 2, 0, 0);
+	if (host == NULL)
+	{
+		fprintf(stderr, "Failed to open ENet host\n");
+		return NULL;
+	}
+	char buf[256];
+	enet_address_get_host_ip(&addrs[server_index], buf, sizeof buf);
+	*peer = enet_host_connect(host, &addrs[server_index], 2, 0);
+	if (*peer == NULL)
+	{
+		fprintf(stderr, "Failed to connect to server on %s:%d\n", buf, addrs[server_index].port);
+		return NULL;
+	}
+	// Wait for connection to succeed
+	ENetEvent event;
+	if (enet_host_service(host, &event, CONNECTION_WAIT_MS) > 0 &&
+		event.type == ENET_EVENT_TYPE_CONNECT)
+	{
+		printf("ENet client connected to %s:%d\n", buf, addrs[server_index].port);
+	}
+	else
+	{
+		fprintf(stderr, "connection failed\n");
+		return NULL;
+	}
+
+	return host;
+}
+
+// Scan for servers on LAN using UDP broadcast
+// Return how many servers we found
+int find_servers(ServerInfo *server_infos, ENetAddress *addrs, int max_servers, int timeout_seconds)
+{
 	ENetSocket scanner = enet_socket_create(ENET_SOCKET_TYPE_DATAGRAM);
 	if (scanner == ENET_SOCKET_NULL)
 	{
 		fprintf(stderr, "Failed to create socket\n");
-		return NULL;
+		return 0;
 	}
 	if (enet_socket_set_option(scanner, ENET_SOCKOPT_BROADCAST, 1) != 0)
 	{
 		fprintf(stderr, "Failed to enable broadcast socket\n");
-		return NULL;
+		return 0;
 	}
 	ENetAddress scanaddr;
 	scanaddr.host = ENET_HOST_BROADCAST;
@@ -122,38 +196,37 @@ ENetHost *start_client(ENetPeer **peer, const int timeout_seconds)
 	sendbuf.dataLength = 1;
 	if (enet_socket_send(scanner, &scanaddr, &sendbuf, 1) != (int)sendbuf.dataLength)
 	{
-		fprintf(stderr, "Failed to scan for LAN server\n");
-		return NULL;
+		fprintf(stderr, "Failed to scan for LAN servers\n");
+		return 0;
 	}
-	// Wait for the reply, which will give us the server address
-	ENetAddress addr;
-	ServerInfo sinfo;
-	bool found = false;
-	for (int i = 0; i < timeout_seconds; i++)
+	// Wait for replies, which will give us server addresses to choose from
+	int sinfo_index = 0;
+	for (int i = 0; i < timeout_seconds && sinfo_index < max_servers; i++)
 	{
 		printf("Scanning for server...\n");
 		ENetSocketSet set;
 		ENET_SOCKETSET_EMPTY(set);
 		ENET_SOCKETSET_ADD(set, scanner);
-		if (enet_socketset_select(scanner, &set, NULL, 0) > 0)
+		while (enet_socketset_select(scanner, &set, NULL, 0) > 0)
 		{
 			ENetBuffer recvbuf;
-			recvbuf.data = &sinfo;
-			recvbuf.dataLength = sizeof sinfo;
-			const int recvlen = enet_socket_receive(scanner, &addr, &recvbuf, 1);
+			recvbuf.data = &server_infos[sinfo_index];
+			recvbuf.dataLength = sizeof server_infos[0];
+			const int recvlen = enet_socket_receive(scanner, &addrs[sinfo_index], &recvbuf, 1);
 			if (recvlen > 0)
 			{
 				if (recvlen != sizeof(ServerInfo))
 				{
 					fprintf(stderr, "Unexpected reply from scan\n");
-					return NULL;
+					return 0;
 				}
-				addr.port = sinfo.port;
+				// The server itself runs on a different port,
+				// so take it from the message
+				addrs[sinfo_index].port = server_infos[sinfo_index].port;
 				char buf[256];
-				enet_address_get_host_ip(&addr, buf, sizeof buf);
-				printf("Found server '%s' at %s:%d\n", sinfo.hostname, buf, addr.port);
-				found = true;
-				break;
+				enet_address_get_host_ip(&addrs[sinfo_index], buf, sizeof buf);
+				printf("Found server '%s' at %s:%d\n", server_infos[sinfo_index].hostname, buf, addrs[sinfo_index].port);
+				sinfo_index++;
 			}
 		}
 		Sleep(1000);
@@ -161,43 +234,10 @@ ENetHost *start_client(ENetPeer **peer, const int timeout_seconds)
 	if (enet_socket_shutdown(scanner, ENET_SOCKET_SHUTDOWN_READ_WRITE) != 0)
 	{
 		fprintf(stderr, "Failed to shutdown listen socket\n");
-		return NULL;
+		return 0;
 	}
 	enet_socket_destroy(scanner);
-	if (!found)
-	{
-		fprintf(stderr, "Server not found\n");
-		return NULL;
-	}
-
-	ENetHost *host = enet_host_create(NULL, 1, 2, 0, 0);
-	if (host == NULL)
-	{
-		fprintf(stderr, "Failed to open ENet host\n");
-		return NULL;
-	}
-	char buf[256];
-	enet_address_get_host_ip(&addr, buf, sizeof buf);
-	*peer = enet_host_connect(host, &addr, 2, 0);
-	if (*peer == NULL)
-	{
-		fprintf(stderr, "Failed to connect to server on %s:%d\n", buf, addr.port);
-		return NULL;
-	}
-	// Wait for connection to succeed
-	ENetEvent event;
-	if (enet_host_service(host, &event, CONNECTION_WAIT_MS) > 0 &&
-		event.type == ENET_EVENT_TYPE_CONNECT)
-	{
-		printf("ENet client connected to %s:%d\n", buf, addr.port);
-	}
-	else
-	{
-		fprintf(stderr, "connection failed\n");
-		return NULL;
-	}
-
-	return host;
+	return sinfo_index;
 }
 
 void send_string(ENetPeer *peer, char *s)
